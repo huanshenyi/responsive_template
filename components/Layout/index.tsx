3d91d421dd7ce2cb1d624872be59dcb0766b1a85
huanshenyi <txy1226052@gmail.com>
2022-09-10 09:05:21 +0900

    テンプレート初期化完了

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,15 +1,26 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { themeChange } from "theme-change";
 
 type LayoutProps = Required<{
   readonly children: ReactElement;
 }>;
 
 const Layout = ({ children }: LayoutProps) => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
   return (
-    <div>
-      <Navbar />
-      <main>{children}</main>
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex justify-center lg:justify-start">
+        <div className="lg:w-[60vw] max-w-screen-lg lg:pt-4 lg:p-4">
+          <Navbar />
+          <main className="flex-1 p-3 md:py-[35]">{children}</main>
+        </div>
+      </div>
+      <Sidebar />
     </div>
   );
 };
