47beae973cbb88f57bc5d94587de0eaad4ca0279
huanshenyi <txy1226052@gmail.com>
2022-09-19 20:18:52 +0900

    閉じるボタンをiconにする

--- src/components/Notifications/Notification.tsx
+++ src/components/Notifications/Notification.tsx
@@ -56,7 +56,14 @@ export const Notification = ({ notification: { id, type, title, message }, onDis
               }}
               className="cursor-pointer"
             >
-              x
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-4 h-4 stroke-current"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
             </div>
           </div>
         </div>
